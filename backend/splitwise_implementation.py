cdb5c5aeec88a514305a40184f9cdd9825b46a64
Aman Bansal <aman0456b@gmail.com>
2023-05-27 19:14:09 -0700

    Fix bug

--- backend/splitwise_implementation.py
+++ backend/splitwise_implementation.py
@@ -1,12 +1,13 @@
 import datetime
 import pytz
+import time
 from entity_interface import EntityInterface
 from splitwise import Splitwise
 from classes import Transaction
 
 
 class SplitwiseAccount(EntityInterface):
-    def __init__(self, account_name, account_type, access_token):
+    def __init__(self):
         super().__init__()
         # Harcoding for now
         self.sObj = Splitwise(
@@ -16,18 +17,18 @@ class SplitwiseAccount(EntityInterface):
         )
         self.id = self.sObj.getCurrentUser().getId()
         self.tz = pytz.timezone("America/Los_Angeles")
-        self.last_updated = datetime.datetime(2023, 5, 2, 13, 0, 0, 0, tzinfo=self.tz)
         self.balance = 0.0
 
     def get_balance(self):
         return self.sObj.getCurrentUser()
 
-    def get_transactions(self, time: datetime.datetime):
+    def get_transactions(self, inp_time):
         transactions = []
         net_balance_delta = 0.0
-        for expense in self.sObj.getExpenses(
-            updated_after=self.last_updated, limit=1000
-        ):
+        update_after = (
+            datetime.datetime.fromtimestamp(inp_time, self.tz) if inp_time else None
+        )
+        for expense in self.sObj.getExpenses(updated_after=update_after, limit=1000):
             is_self_involved = False
             eu = None
             for expense_user in expense.getUsers():
@@ -36,19 +37,14 @@ class SplitwiseAccount(EntityInterface):
                     is_self_involved = True
             if not is_self_involved:
                 continue
-            ct = datetime.datetime.strptime(
-                expense.getCreatedAt(), "%Y-%m-%dT%H:%M:%S%z"
-            )
-            ut = datetime.datetime.strptime(
-                expense.getUpdatedAt(), "%Y-%m-%dT%H:%M:%S%z"
-            )
-            ct = ct.replace(tzinfo=pytz.utc).astimezone(self.tz)
-            ut = ut.replace(tzinfo=pytz.utc).astimezone(self.tz)
+            ct = time.strptime(expense.getCreatedAt(), "%Y-%m-%dT%H:%M:%S%z")
+            ut = time.strptime(expense.getUpdatedAt(), "%Y-%m-%dT%H:%M:%S%z")
+            # ct = ct.replace(tzinfo=pytz.utc).astimezone(self.tz)
+            # ut = ut.replace(tzinfo=pytz.utc).astimezone(self.tz)
             transaction = Transaction(
                 "Splitwise", eu.net_balance, expense.getDescription(), ut, ct
             )
             transactions.append(transaction)
             net_balance_delta += float(eu.net_balance)
         self.balance += net_balance_delta
-        self.last_updated = datetime.datetime.now(tz=self.tz)
         return transactions
--- backend/splitwise_test.py
+++ backend/splitwise_test.py
@@ -1,8 +1,6 @@
 from splitwise_implementation import SplitwiseAccount
 
 if __name__ == "__main__":
-    s = SplitwiseAccount(
-        "Splitwise", "Splitwise", "5VroAWaqz7NV8TtdosKf3i7LgPwXzTvhnsiyQzwu"
-    )
+    s = SplitwiseAccount()
     print(s.get_balance())
     print(s.get_transactions(None))
